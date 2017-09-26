ec3bf3feb9ecfe4a40e2ebfae3566e9d196e3041
Nikita Laptev <nikita.laptev98@inbox.ru>
2017-09-26 15:20:06 +0300

    end lab.work

--- client-remote.js
+++ client-remote.js
@@ -10,15 +10,27 @@ client.connect({port: port, host: '127.0.0.1'}, () => {
     client.write(string);
 });
 client.on('data', (data, err) => {
-    if (data === good)
+    if (data === good) {
         sendCopy();
+        sendEncode();
+        sendDecode();
+        client.destroy();
+    }
     else if (data === bad)
         client.destroy();
 });
 client.on('close', () => {
     console.log('Client disconnected');
 });
-function sendCopy()
-{
-    client.write('COPY e:\\SpecForLabs\\1.txt e:\\SpecForLabs');
+
+function sendCopy() {
+    client.write('COPY e:\\SpecForLabs\\1.txt e:\\SpecForLabs\\copy1.txt');
+}
+
+function sendEncode() {
+    client.write('ENCODE e:\\SpecForLabs\\1.txt e:\\SpecForLabs\\encode1.txt 1234');
+}
+
+function sendDecode() {
+    client.write('DECODE e:\\SpecForLabs\\encode1.txt e:\\SpecForLabs\\1.txt 1234');
 }
--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const net = require('net');
 const fs = require('fs');
-const path = require ('path');
+const path = require('path');
+const crypto = require('crypto');
+
 const port = 8124;
+
 const remoteString = 'REMOTE';
+const copyString = 'COPY';
+const encodeString = 'ENCODE';
+const decodeString = 'DECODE';
+const cryptoString = 'aes-128-cbc';
 const good = 'ACK';
 const bad = 'DEC';
+
 const logger = fs.createWriteStream('client_id.log');
 
 const server = net.createServer((client) => {
@@ -15,15 +23,26 @@ const server = net.createServer((client) => {
         if (err) console.error(err);
         else if (data === remoteString)
             client.write(good)
-        else
-        {
+        else {
             let strings = data.split(' ');
-            let file = fs.createReadStream(strings[1]);
-            const fileName = strings[2]+'\\copy' + path.basename(strings[1]);
-            console.log(fileName);
-            let copyFile = fs.createWriteStream(fileName);
-            file.pipe(copyFile)
-            client.write(bad);
+            if (strings[0] === copyString) {
+                let file = fs.createReadStream(strings[1]);
+                let copyFile = fs.createWriteStream(strings[2]);
+                file.pipe(copyFile)
+                //client.write(bad);
+            }
+            else if (strings[0] === encodeString) {
+                let file = fs.createReadStream(strings[1]);
+                let encryptFile = fs.createWriteStream(strings[2]);
+                let cryptoStream = crypto.createCipher(cryptoString, strings[3]);
+                file.pipe(cryptoStream).pipe(encryptFile);
+            }
+            else if (strings[0] === decodeString) {
+                let file = fs.createReadStream(strings[1]);
+                let decryptFile = fs.createWriteStream(strings[2]);
+                let cryptoStream = crypto.createDecipher(cryptoString, strings[3]);
+                file.pipe(cryptoStream).pipe(decryptFile);
+            }
         }
     });
     client.on('end', () => {
